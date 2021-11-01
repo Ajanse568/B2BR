046f6daee308c93b2fc3861e80158ef55a5ede7f
root <root@Alwin.alwin.com>
2021-11-01 13:33:21 +0100

    Last part

--- monitoring.sh
+++ monitoring.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+fudisk=$(df -Bm | awk '{fd+=$2} END {print fd}')
+avdisk=$(df -Bm | awk '{ad+=$4} END {print ad}')
+pdisk=$((avdisk/(fudisk/100)))
+fram=$(free -m | awk 'NR==2 {print $2}')
+avram=$(free -m | awk 'NR==2 {print $4}')
+pram=$((avram/(fram/100)))
+
 echo -e "Attention! Important message from your servermaster!"
 echo ""
-echo -e "	-----------------------------Message----------------------------------"
-echo -e "	Here's some handy information about our server:"
+echo -e " -----------------------------Message----------------------------------"
+echo -e " Here's some handy information about our server:"
 echo ""
-echo -e "	Architecture: 	"
-echo -e "	Physical CPU's: "
-echo -e "	vCPU's: 	"
-echo -e "	Memory usage: 	" 
-echo -e "	Disk usage: 	"
-echo -e "	CPU load: 	"
-echo -e "	Last boot: 	"
-echo -e "	LVM: 		"
-echo -e "	Network:	IPv4 "`hostname -I`" MAC "
+echo -e " Architecture:   "`uname -a`
+echo -e " Physical CPU's: "`cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l`
+echo -e " vCPU's: 	 "`cat /proc/cpuinfo | grep processor | wc -l`
+echo -e " RAM: 		 $avram MB $pram% Available" 
+echo -e " Disk Space: 	$avdisk MB $pdisk% Available"
+echo -e " CPU load: 	"
+echo -e " Last boot: 	"
+echo -e " LVM: 		"
+echo -e " Network:	IPv4 "`hostname -I`" MAC "

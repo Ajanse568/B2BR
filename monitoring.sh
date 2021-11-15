cbc7e8e786e46160b2fb17bef983e300e16f6809
Ajanse568 <89343251+Ajanse568@users.noreply.github.com>
2021-11-15 16:54:50 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -5,18 +5,22 @@ pdisk=$((avdisk/(fudisk/100)))
 fram=$(free -m | awk 'NR==2 {print $2}')
 avram=$(free -m | awk 'NR==2 {print $4}')
 pram=$((avram/(fram/100)))
+name=$(uname -a)
+pcpu=$(cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l)
+vcpu=$(cat /proc/cpuinfo | grep processor | wc -l)
+net=$(hostname -I)
 
-echo -e "Attention! Important message from your servermaster!"
-echo ""
-echo -e " -----------------------------Message----------------------------------"
-echo -e " Here's some handy information about our server:"
-echo ""
-echo -e " Architecture:   "`uname -a`
-echo -e " Physical CPU's: "`cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l`
-echo -e " vCPU's: 	 "`cat /proc/cpuinfo | grep processor | wc -l`
-echo -e " RAM: 		 $avram MB $pram% Available" 
-echo -e " Disk Space: 	$avdisk MB $pdisk% Available"
-echo -e " CPU load: 	"
-echo -e " Last boot: 	"
-echo -e " LVM: 		"
-echo -e " Network:	IPv4 "`hostname -I`" MAC "
+wall  "Attention! Important message from your servermaster!
+
+      -----------------------------Message----------------------------------
+      Here's some handy information about our server:
+ 
+      Architecture: $name  
+      Physical CPU's: $pcpu
+      vCPU's: $vcpu
+      RAM: 		 $avram MB $pram% Available
+      Disk Space: 	$avdisk MB $pdisk% Available
+      CPU load: 	
+      Last boot: 	
+      LVM: 		
+      Network:	IPv4 $net MAC "
